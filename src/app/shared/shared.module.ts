b7c8e9ae197da02b9f0cdd6fed01f0a5e668fc1f
Stiflerzak <hilarymwendia97@gmail.com>
2023-10-22 23:03:46 +0300

    Added graph

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -11,13 +11,15 @@ import {MatMenuModule} from '@angular/material/menu';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
-
+import { AreaComponent } from './widgets/area/area.component';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
-    SidebarComponent
+    SidebarComponent,
+    AreaComponent
 
   ],
   imports: [
@@ -29,12 +31,15 @@ import { RouterModule } from '@angular/router';
     FlexLayoutModule,
     MatMenuModule,
     MatListModule,
-    RouterModule
+    RouterModule,
+    HighchartsChartModule,
+    
   ],
   exports: [
     HeaderComponent,
     FooterComponent,
-    SidebarComponent
+    SidebarComponent,
+    AreaComponent
 
   ]
 })
